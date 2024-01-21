99fd7fea5f8c825324baf022ab1cd96e69734221
roylee <roylee@iamagod.sg>
2024-01-21 16:49:00 +0800

    add newline

--- srcs/print.c
+++ srcs/print.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 16:11:17 by roylee            #+#    #+#             */
-/*   Updated: 2024/01/21 16:11:42 by roylee           ###   ########.fr       */
+/*   Updated: 2024/01/21 16:48:48 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void	print_stack(t_stack *stack)
 	while (node)
 	{
 		printf("node->idx: %d\n", node->idx);
-		printf("node->val: %d\n", node->val);
+		printf("node->val: %d\n\n", node->val);
 		node = node->next;
 	}
 }
