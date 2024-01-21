237ecf960171be25c39ef56dab25cba83971c505
roylee <roylee@iamagod.sg>
2024-01-21 16:28:43 +0800

    typo

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 14:24:20 by roylee            #+#    #+#             */
-/*   Updated: 2024/01/21 16:27:19 by roylee           ###   ########.fr       */
+/*   Updated: 2024/01/21 16:28:35 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,7 +50,7 @@ void	init_stacks(t_data *data, int argc, char **argv)
 	stack_a->size = argc - 1;
 	stack_b->size = 0;
 	stack_a->head = create_node(0, ft_atoi(argv[1]));
-	stack_a->tail = create_node(argc - 1, t_atoi(argv[argc - 1]));
+	stack_a->tail = create_node(argc - 1, ft_atoi(argv[argc - 1]));
 }
 
 t_data	*init_data(int argc)
