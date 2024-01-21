de1f8b1534738b42dbe507c0c77a072290bec3ec
roylee <roylee@iamagod.sg>
2024-01-21 14:10:05 +0800

    update prototype

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -2,14 +2,15 @@
 # define PUSH_SWAP_H
 
 # include <limits.h>
+# include <stdio.h>
 
 # include "libft.h"
 
 /*
 check.c
 */
-void	check_dup(int argc, char **argv);
-void	check_args(int argc, char **argv);
+static int		check_dup(int argc, char **argv);
+void			check_args(int argc, char **argv);
 
 /*
 error.c
