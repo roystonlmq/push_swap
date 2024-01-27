6d9d3d03ef31633875c1b40a76eb87aa590f70a7
roylee <royston@42.fr>
2024-01-27 23:07:07 +0800

    fix sort3 logic

--- srcs/sort/sort_3.c
+++ srcs/sort/sort_3.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 00:16:42 by roylee            #+#    #+#             */
-/*   Updated: 2024/01/24 00:19:42 by roylee           ###   ########.fr       */
+/*   Updated: 2024/01/27 23:04:03 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,36 @@ sort_3:
 
 Deals with the following permutations (3! = 6)
 [1, 2, 3] - no swaps
-[1, 3, 2] - sa
-[2, 1, 3] - ra
-[2, 3, 1] - sa, ra
-[3, 1, 2] - rra
-[3, 2, 1] - sa, rra
+[1, 3, 2] - sa -> [3, 1, 2] ra -> [1, 2, 3]
+[2, 1, 3] - sa -> [1, 2, 3]
+[2, 3, 1] - rra -> [1, 2, 3]
+[3, 1, 2] - ra -> [1, 2, 3]
+[3, 2, 1] - sa -> [2, 3, 1] rra -> [1, 2, 3]
 */
 void	sort_3(t_data *data)
 {
-	if (data->stack_a->head->val > data->stack_a->head->next->val)
+	int		a;
+	int		b;
+	int		c;
+
+	a = data->stack_a->head->val;
+	b = data->stack_a->head->next->val;
+	c = data->stack_a->head->next->next->val;
+	printf("a: %d, b: %d, c: %d\n", a, b, c);
+	if (a < b && b > c && a < c)
 	{
-		if (data->stack_a->head->val < data->stack_a->head->next->next->val)
-			sa(data->stack_a);
-		else
-			rra(data->stack_a);
+		sa(data->stack_a);
+		ra(data->stack_a);
 	}
-	else if (data->stack_a->head->val < data->stack_a->head->next->val)
+	else if (a > b && b < c && a < c)
+		sa(data->stack_a);
+	else if (a < b && b > c && a > c)
+		rra(data->stack_a);
+	else if (a > b && b < c && a > c)
+		ra(data->stack_a);
+	else if (a > b && b > c && a > c)
 	{
-		if (data->stack_a->head->next->val > 
-				data->stack_a->head->next->next->val)
-			ra(data->stack_a);
-		else
-		{
-			sa(data->stack_a);
-			ra(data->stack_a);
-		}
+		sa(data->stack_a);
+		rra(data->stack_a);
 	}
 }
