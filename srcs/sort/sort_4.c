4759da0ceda117bfb7aa0b8fa430ca46bde5eb23
roylee <royston@42.fr>
2024-01-28 00:42:08 +0800

    add sort4, TODO

--- srcs/sort/sort_4.c
+++ srcs/sort/sort_4.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 23:17:32 by roylee            #+#    #+#             */
-/*   Updated: 2024/01/27 23:51:44 by roylee           ###   ########.fr       */
+/*   Updated: 2024/01/28 00:40:56 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,6 @@ sort 4 numbers using 2 stacks
 */
 void	sort_4(t_data *data)
 {
-	int		i;
-	int		min;
-	int		max;
-
-	i = 0;
-	min = get_min(data->stack_a);
-	max = get_max(data->stack_a);
-	while (i < 4)
-	{
-		if (data->stack_a->head->val == min || data->stack_a->head->val == max)
-			pb(data);
-		else
-			ra(data->stack_a);
-		i++;
-		printf("%d \n", i);
-	}
-	sort_3(data);
-	pa(data);
+	(void) data;
+	printf("TODO");
 }
