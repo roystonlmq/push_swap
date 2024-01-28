cfaae458dae80347540278552e200fefa92ee097
roylee <royston@42.fr>
2024-01-28 14:42:56 +0800

    add flag to get_min_node, add find_idx func

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,29 +6,50 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/27 23:48:13 by roylee            #+#    #+#             */
-/*   Updated: 2024/01/28 00:49:17 by roylee           ###   ########.fr       */
+/*   Updated: 2024/01/28 14:42:26 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/*
+Function to look for index
+*/
+int		find_idx(t_stack *stack, int idx)
+{
+	t_node	*node;
+	int		idx;
+
+	idx = 0;
+	node = stack->head;
+	while (node)
+	{
+		if (node->idx == idx)
+			return (idx);
+		idx++;
+		node = node->next;
+	}
+	return (-1);
+}
+
 t_node	*get_min_node(t_stack *stack)
 {
 	t_node	*min_node;
 	t_node	*node;
-	int		min;
+	int		min_flag;
 
 	node = stack->head;
-	min = node->val;
+	min_node = node;
+	min_flag = 0;
 	while (node)
 	{
-		if (node->val < min && node->idx == -1)
+		if ((node->val <= min_node->val || !min_flag) && node->idx == -1)
 		{
-			min = node->val;
 			min_node = node;
+			min_node->val = node->val;
+			min_flag = 1;
 		}
 		node = node->next;
-		printf("test");
 	}
 	return (min_node);
 }
