b6ba833a239d3736c67441826add55b92a6fc60e
roylee <roylee@iamagod.sg>
2024-01-29 21:48:56 +0800

    clean up commented code

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 14:24:02 by roylee            #+#    #+#             */
-/*   Updated: 2024/01/29 20:26:16 by roylee           ###   ########.fr       */
+/*   Updated: 2024/01/29 21:48:45 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,5 @@ int	main(int argc, char **argv)
 	if (argc == 2)
 		free_split(args);
 	reindex_stack(data->stack_a);
-	// rot(data->stack_a);
-	// rot_data(data, &rot);
-	// rrot(data->stack_a);
-	// swap(data->stack_a);
-	// push(data->stack_b, data->stack_a);
-	// sa(data->stack_a);
-	// rra(data->stack_a);
-	// sort_3(data);
 	sort_flow(data);
-	// ra(data->stack_a);
-	// print_stack(data->stack_a);
-	ft_printf(".................................. \n");
-	// print_stack(data->stack_b);
-	// sort(&stack_a, &stack_b);
 }
