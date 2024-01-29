cad664f0bf2df5eb648524dacfcdafd14320ad95
roylee <roylee@iamagod.sg>
2024-01-29 21:47:58 +0800

    update check.c

--- srcs/check.c
+++ srcs/check.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 01:06:52 by roylee            #+#    #+#             */
-/*   Updated: 2024/01/29 20:19:11 by roylee           ###   ########.fr       */
+/*   Updated: 2024/01/29 21:44:20 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,14 +31,29 @@ static int	check_dup(int argc, char **argv)
 		while (i-- > 0)
 		{
 			if (arr[len] == arr[i])
-				ft_error();
+				return (-1);
 			if (arr[len] > INT_MAX || arr[len] < INT_MIN)
-				ft_error();
+				return(-1);
 		}
 	}
 	return (0);
 }
 
+static int	check_num(char *n)
+{
+	int	i;
+
+	i = 0;
+	if (n[0] == '-')
+		i++;
+	while (n[i])
+	{
+		if (ft_isdigit(n[i]) == 0)
+			return (-1);
+		i++;
+	}
+	return (0);
+}
 /*
 check_args:
 
@@ -50,9 +65,11 @@ Checks:
 char	**check_args(int argc, char **argv)
 {
 	int		i;
-	int		j;
+	int		err;
 	char	**args;
 
+	err = 0;
+	i = 0;
 	if (argc == 1)
 		ft_error();
 	if (argc == 2)
@@ -64,17 +81,16 @@ char	**check_args(int argc, char **argv)
 	}
 	while (args[i])
 	{
-		j = ft_strlen(args[i]);
-		while (j-- > 0)
-		{
-			if (ft_isdigit(args[i][j]) == 0)
-				ft_error();
-			if (args[i][j] == '-' && j != 0)
-				ft_error();
-			if (check_dup(argc, args) > 0)
-				ft_error();
-		}
+		if (check_num(args[i]) == -1)
+			err = 1;
+		if (check_dup(argc, args) == -1)
+			err = 1;
 		i++;
+	}
+	if (err == 1)
+	{
+		free_split(args);
+		ft_error();
 	}
 	return (args);
 }
