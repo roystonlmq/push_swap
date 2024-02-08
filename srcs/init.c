30e76e1012c093b3045feff3c1ad6c65a70b36ae
roylee <roylee@iamagod.sg>
2024-02-08 22:42:00 +0800

    change all mallocs to callocs, and change condition to create node, simplify

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 14:24:20 by roylee            #+#    #+#             */
-/*   Updated: 2024/02/02 01:16:04 by roylee           ###   ########.fr       */
+/*   Updated: 2024/02/08 22:37:53 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_node	*create_node(int val)
 {
 	t_node	*node;
 
-	node = (t_node *)malloc(sizeof(t_node));
+	node = (t_node *)ft_calloc(1, sizeof(t_node));
 	if (!node)
 		ft_error();
 	node->next = NULL;
@@ -35,24 +35,25 @@ void	init_stacks(t_data *data, int stack_size, int argc, char **args)
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 
-	stack_a = (t_stack *)malloc(sizeof(t_stack));
+	stack_a = (t_stack *)ft_calloc(1, sizeof(t_stack));
 	if (!stack_a)
 		ft_error();
-	stack_b = (t_stack *)malloc(sizeof(t_stack));
+	stack_b = (t_stack *)ft_calloc(1, sizeof(t_stack));
 	if (!stack_b)
 		ft_error();
 	stack_b->head = NULL;
 	stack_a->size = stack_size;
 	stack_b->size = 0;
-	if (argc == 2)
-	{
-		stack_a->size = stack_size;
-		stack_a->head = create_node(ft_atoi(args[0]));
-	}
-	else
+	if ((argc == 2 && stack_a->size > 1) || argc > 2)
 		stack_a->head = create_node(ft_atoi(args[0]));
 	if (stack_a->size <= 1)
+	{
+		free(stack_a);
+		free(stack_b);
+		free(data);
+		free_split(args);
 		exit(0);
+	}
 	data->stack_a = stack_a;
 	data->stack_b = stack_b;
 }
@@ -61,7 +62,7 @@ t_data	*init_data(int argc)
 {
 	t_data	*data;
 
-	data = (t_data *)malloc(sizeof(t_data));
+	data = (t_data *)ft_calloc(1, sizeof(t_data));
 	if (!data)
 		ft_error();
 	data->argc = argc;
