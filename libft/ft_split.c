d0bc2176582b53da5d0b31f81ed8a66799f3fcfe
roylee <roylee@iamagod.sg>
2024-02-08 22:39:21 +0800

    add free func to ft_split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,106 +3,90 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: roylee <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: roylee <roylee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 18:22:50 by roylee            #+#    #+#             */
-/*   Updated: 2023/09/12 21:59:33 by roylee           ###   ########.fr       */
+/*   Updated: 2024/02/08 22:12:09 by roylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	check_sep(char s, char c)
-{
-	return (s == c);
-}
-
-static int	count_words(char const *s, char c)
+static int	ft_count_word(char const *s, char c)
 {
 	int	i;
-	int	count;
+	int	word;
 
 	i = 0;
-	count = 0;
+	word = 0;
 	while (s[i])
 	{
-		while (s[i] && check_sep(s[i], c))
-			i++;
-		if (s[i])
-			count++;
-		while (s[i] && !check_sep(s[i], c))
+		if (s[i] != c)
+		{
+			word++;
+			while (s[i] != c && s[i])
+				i++;
+		}
+		else
 			i++;
 	}
-	return (count);
+	return (word);
 }
 
-static int	ft_strlen_sep(char const *s, char c)
+static int	ft_size_word(char const *s, char c, int i)
+{
+	int	size;
+
+	size = 0;
+	while (s[i] != c && s[i])
+	{
+		size++;
+		i++;
+	}
+	return (size);
+}
+
+static void	ft_free(char **strs, int j)
+{
+	while (j-- > 0)
+		free(strs[j]);
+	free(strs);
+}
+
+char	**split(char const *s, char c, char **strs, int word)
 {
 	int	i;
+	int	j;
+	int	size;
 
 	i = 0;
-	while (s[i] && !check_sep(s[i], c))
-		i++;
-	return (i);
-}
-
-static char	*ft_word(char const *s, char c)
-{
-	int		wlen;
-	int		i;
-	char	*word;
-
-	i = 0;
-	wlen = ft_strlen_sep(s, c);
-	word = (char *)malloc(wlen + 1);
-	if (!word)
-		return (NULL);
-	while (i < wlen)
+	j = -1;
+	while (++j < word)
 	{
-		word[i] = s[i];
-		i++;
+		while (s[i] == c)
+			i++;
+		size = ft_size_word(s, c, i);
+		strs[j] = ft_substr(s, i, size);
+		if (!strs[j])
+		{
+			ft_free(strs, j);
+			return (0);
+		}
+		i += size;
 	}
-	word[i] = 0;
-	return (word);
+	strs[j] = 0;
+	return (strs);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**ret;
-	char	*str;
-	int		i;
+	int		word;
+	char	**strs;
 
-	i = 0;
-	str = (char *)s;
-	ret = (char **)malloc(sizeof(char *) * (count_words(s, c) + 1));
-	if (!ret)
-		return (NULL);
-	while (*str)
-	{
-		while (*str && check_sep(*str, c))
-			str++;
-		if (*str)
-		{
-			ret[i] = ft_word(str, c);
-			i++;
-		}
-		while (*str && !check_sep(*str, c))
-			str++;
-	}
-	ret[i] = 0;
-	return (ret);
+	word = ft_count_word(s, c);
+	strs = (char **)malloc((word + 1) * sizeof(char *));
+	if (strs == NULL)
+		return (0);
+	strs = split(s, c, strs, word);
+	return (strs);
 }
-
-/*
-int	main(void)
-{
-	char s[] = "lorem ipsum dolor sit amet, consectetur adipisc    ing elit. 
-Sed non risus. Suspendisse";
-	char c = ' ';
-	char	**res = ft_split(s, c);
-	for (int i = 0; res[i]; i++)
-	{
-		printf("%s\n", res[i]);
-	}
-}
-*/
